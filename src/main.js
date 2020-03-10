548837144fdfb8a1a0943855684dd994b0ede2e0
Artur <Artur.Hoppner@hotmail.com>
2020-03-10 10:28:41 +0100

    Added assets and data in main.js

--- src/main.js
+++ src/main.js
@@ -6,5 +6,14 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+
+  data: () => ({
+    cardlist: [
+      {id: 1, cardnumber: "5776 5676 5894 6544", cardholdername: "Hungry Hippo", month: "01", year: "23", vendor: 'evil',},
+      {id: 2, cardnumber: "6448 5642 7665 8766", cardholdername: "God Zilla", month: "09", year: "20", vendor: "bitcoin",},
+      {id: 3, cardnumber: "1234 2342 5555 1432", cardholdername: "keyser soze", month: "02", year: "21", vendor: "ninja",},
+    ]
+  }),
+
   render: h => h(App)
 }).$mount('#app')
